399eda9b72701bc21bc620b9ed08d3067ed94dc7
adwirawan <61340371+adwirawan@users.noreply.github.com>
2021-08-21 15:07:16 +0700

    tried to fix landing page style

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -5,6 +5,8 @@ import doctorPic1 from "../assets/lifesavers.png";
 import doctorPic2 from "../assets/plant.png";
 import {Link} from 'react-router-dom';
 import Navbar from '../components/Navbar/Navbar';
+import Grid from '@material-ui/core/Grid';
+import doctor from '../assets/doctor.png'
 
 export default function Landing() {
   return (
@@ -15,23 +17,33 @@ export default function Landing() {
 
       <Navbar />
 
-      <img src={doctorPic1} className='img-1'/>
-      <img src={doctorPic2} className='img-2'/>
-
-      <div className='buttons'>
-        <Link to='/booking'>
-          <button className='btn-choose-1'>
-            <h2 className='btn-text'>COVID TEST BOOKING</h2>
+      <Grid 
+        container
+        direction="row"
+        justifyContent="space-evenly"
+        alignItems="center"
+      >
+        <Grid item xs={6} sm={3}>
+          <img src={doctor} className='img-1'/>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Link to='/booking'>
+            <button className='btn-choose-1'>
+              <h2 className='btn-text'>COVID TEST BOOKING</h2>
+            </button>
+          </Link>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <button className='btn-choose-2'>
+            <a href ='https://portal.cvms.vic.gov.au/'>
+              <h2 className='btn-text'>BOOK A VACCINATION</h2>
+            </a>
           </button>
-        </Link>
-
-        <button className='btn-choose-2'>
-          <a href ='https://portal.cvms.vic.gov.au/'>
-            <h2 className='btn-text'>BOOK A VACCINATION</h2>
-          </a>
-        </button>
-
-      </div>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          {/* <img src={doctorPic2} className='img-2'/> */}
+        </Grid>
+      </Grid>
     </div>
   )
 }
