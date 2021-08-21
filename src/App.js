62a8d0ea2e04c30e8fbbea860571fe70c2fba0a9
green-ham-and-eggs <tabitha.ayudya@gmail.com>
2021-08-21 13:23:37 +1000

    added user landing page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import HospitalAvailability from './pages/HospitalAvailability';
 import UserLogin from './pages/UserLogin';
+import Landing from './pages/Landing';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <UserLogin />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <Landing />
+          </Route>
+          <Route exact path="/login">
+            <UserLogin />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import './Landing.css';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import doctorPic1 from "../assets/lifesavers.png";
+import doctorPic2 from "../assets/plant.png";
+
+export default function Landing() {
+  return (
+    <div className="main">
+      <div className="container">
+        <h1 className='title'># Me Is Covid Free</h1>
+      </div>
+
+      <img src={doctorPic1} className='img-1'/>
+      <img src={doctorPic2} className='img-2'/>
+
+      <div className='buttons'>
+        <button className='btn-choose-1'>
+          <h2 className='btn-text'>COVID TEST BOOKING</h2>
+        </button>
+
+        <button className='btn-choose-2'>
+          <a href ='https://portal.cvms.vic.gov.au/'>
+            <h2 className='btn-text'>BOOK A VACCINATION</h2>
+          </a>
+        </button>
+
+      </div>
+    </div>
+  )
+}
