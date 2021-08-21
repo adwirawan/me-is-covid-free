4f02818d692116ce409690b5a5cff707fdae75e9
adwirawan <61340371+adwirawan@users.noreply.github.com>
2021-08-21 12:35:49 +0700

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 import HospitalAvailability from './pages/HospitalAvailability';
+import BookingConfirmation from './pages/BookingConfirmation';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <HospitalAvailability />
+
+      <Navbar />
+
+      {/* <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ BookingConfirmation } />
+          <Route path="/hospital/availability" component={ HospitalAvailability } />
+        </Switch>
+      </BrowserRouter> */}
     </div>
   );
 }
